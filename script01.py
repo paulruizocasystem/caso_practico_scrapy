297199291ac864ee35c5adf850db6bfd1d4a380d
paulruizocasystem <paul.ruiz@ocasystem.com>
2024-07-22 20:02:30 -0500

    mostrando la lista de las maquinas de la pagina web https://www.vulnhub.com/ , se limpio la data obtenida y se mostro en una lista con un bucle for

--- script01.py
+++ script01.py
@@ -1,4 +1,24 @@
-import re
-import colorama import Fore
-import requests
+import re #expresiones regulares#
+from colorama import Fore #colores#
+import requests #peticiones hhttp a paginas web#
+
+website = "https://www.vulnhub.com/"
+resultado = requests.get(website)
+content = resultado.text
+#print(content)
+
+patron = r"/entry/[\w-]*"
+maquinas_repetidas = re.findall(patron, str(content))
+#print(maquinas_repetidas)
+sin_duplicados = list(set(maquinas_repetidas))
+#print(sin_duplicados)
+
+maquinas_final = []
+
+for i in sin_duplicados:
+    nombre_maquinas = i.replace("/entry/", "")
+    maquinas_final.append(nombre_maquinas)
+    print(nombre_maquinas)
+#print(maquinas_final)
+
 
